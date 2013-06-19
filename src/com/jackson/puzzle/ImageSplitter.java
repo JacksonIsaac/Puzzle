6e43d95b5d10129a86ee17e32ab455c1ec605c08
JacksonIsaac <jacksonisaac2008@gmail.com>
2013-06-19 15:25:34 +0530

    Update ImageSplitter.java

--- src/com/jackson/puzzle/ImageSplitter.java
+++ src/com/jackson/puzzle/ImageSplitter.java
@@ -85,13 +85,6 @@ public class ImageSplitter extends Activity implements OnClickListener {
 			yCoord += chunkHeight;
 		}
 
-		/*
-		 * Now the chunkedImages has all the small image chunks in the form of
-		 * Bitmap class. You can do what ever you want with this chunkedImages
-		 * as per your requirement. I pass it to a new Activity to show all
-		 * small chunks in a grid for demo. You can get the source code of this
-		 * activity from my Google Drive Account.
-		 */
 
 		// Start a new activity to show these chunks into a grid
 		Intent intent = new Intent(this,
